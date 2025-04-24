1dab990a34a6bab30d348648c8dcdd13213333d8
DatHuynhCoder <htdat2004@gmail.com>
2025-04-24 13:15:55 +0700

    api getdishbyid + getcommentsbydishid

--- routes/user/displayData.route.js
+++ routes/user/displayData.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllCategory, getAllComments, getDishes, getTopRatingDishes } from '../../controllers/user/displayData.controller.js';
+import { getAllCategory, getAllComments, getCommentsByDishId, getDishById, getDishes, getTopRatingDishes } from '../../controllers/user/displayData.controller.js';
 
 const displayDataRouter = express.Router();
 
@@ -14,5 +14,11 @@ displayDataRouter.get('/toprating', getTopRatingDishes)
 
 //get all dishes, get dishes by category
 displayDataRouter.get('/dishes', getDishes)
+
+//get dish by Id
+displayDataRouter.get('/dish/:id', getDishById)
+
+//get comments by dishId
+displayDataRouter.get('/commentsbydish/:id', getCommentsByDishId)
 
 export default displayDataRouter;
