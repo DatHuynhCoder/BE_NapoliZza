f48891bec5967441f4ce870242a66228920f86da
DatHuynhCoder <htdat2004@gmail.com>
2025-05-21 22:15:47 +0700

    Delete api test

--- server.js
+++ server.js
@@ -42,50 +42,6 @@ app.get('/', (req, res) => {
   res.send('<h1>Welcome to Ours Server</h1>');
 })
 
-//API to TEST
-//1 Get all accounts information
-app.get('/accounts', async (req, res) => {
-  try {
-    const accounts = await Account.find({});
-    res.status(200).json({ success: true, data: accounts });
-  } catch (error) {
-    console.error("Error in get accounts: ", error.message);
-    return res.status(500).json({ success: false, message: "Server error" });
-  }
-})
-
-//2 Create new account
-app.post('/accounts', async (req, res) => {
-  try {
-    const newAccount = await Account.create(req.body);
-    res.status(201).json({ success: true, data: newAccount });
-  } catch (error) {
-    console.error("Error in create account: ", error.message);
-    return res.status(500).json({ success: false, message: "Server error" });
-  }
-})
-
-//3 create many accounts
-app.post('/accounts/many', async (req, res) => {
-  try {
-    const newAccounts = await Account.insertMany(req.body);
-    res.status(201).json({ success: true, data: newAccounts });
-  } catch (error) {
-    console.error("Error in create many accounts: ", error.message);
-    return res.status(500).json({ success: false, message: "Server error" });
-  }
-})
-
-//4 delete all accounts
-app.delete('/accounts', async (req, res) => {
-  try {
-    await Account.deleteMany({});
-    res.status(200).json({ success: true, message: "Delete all accounts successfully" });
-  } catch (error) {
-    console.error("Error in delete all accounts: ", error.message);
-    return res.status(500).json({ success: false, message: "Server error" });
-  }
-})
 
 //CUSTOMER API HERE
 //customer comment api
